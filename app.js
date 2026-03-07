d3b8c763ec8794d5568ea90b5da1f4d25fa00d5a
withmychild <withmychild.dz@gmail.com>
2026-03-07 07:43:39 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-firebase.auth().onAuthStateChanged(function(user) {
-
-if (!user) {
-
-window.location = "login.html";
-
-}
-
-});
-
-document.getElementById("logout-btn").onclick=function(){
-
-firebase.auth().signOut()
-.then(()=>{
-
-window.location="login.html"
-
-})
-
-}
 // Firebase configuration from google-services.json
 const firebaseConfig = {
     apiKey: "AIzaSyDIWizvGK3AxdRELeCRzk7rJ3ONzjT-0Zk",
@@ -449,4 +429,3 @@ if (articleSearch) articleSearch.oninput = renderArticles;
 loadMilestones();
 loadVaccinations();
 loadArticles();
-
